2c7e7611501551b459f4bcf8fb93f8c2a887e696
Robin de Vries <robin.de_vries@atraveo.de>
2016-09-30 13:43:29 +0200

    testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    getPixelArray = function (r,g,b,a = 255) {
+    getPixelArray = function (r,g,b,a = 0) {
         var data = [];
         data[0] = r;
         data[1] = g;
@@ -9,4 +9,16 @@
         data[3] = a;
         return data;
     };
+
+    var frontCanvas = $('#front');
+    var frontContext = frontCanvas.getContext('2d');
+
+    frontContext.fillStyle = "rgb(200,0,0)";
+    frontContext.fillRect (10, 10, 50, 50);
+    var imageData = frontContext.createImageData(1,1);
+    imageData.data = getPixelArray(255, 0, 0);
+    imageData.putImageData(imageData, 100, 100);
+    imageData.data = getPixelArray(0, 0, 0, 0);
+    imageData.putImageData(imageData, 100, 100);
+    console.log("Test");
 }());
