25395f59613c0214062827d4a0557d8f5107293e
Robin de Vries <robin.de_vries@atraveo.de>
2016-09-30 13:32:19 +0200

    Added canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 (function () {
     'use strict';
+
+    getPixelArray = function (r,g,b,a = 255) {
+        var data = [];
+        data[0] = r;
+        data[1] = g;
+        data[2] = b;
+        data[3] = a;
+        return data;
+    };
 }());
